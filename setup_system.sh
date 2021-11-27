7db63d3ca61051f8d78bfde8a8f8f60113ff9c48
Ray Vermey <rayvermey@gmail.com>
2021-11-27 18:42:25 +0100

    changed setup system

--- setup_system.sh
+++ setup_system.sh
@@ -1,2 +1,26 @@
-pacman -S --needed  < LAPTOP_PACMAN_PACKAGES.lst
+sudo pacman -Syu
+
+#git clone https://aur.archlinux.org/yay
+#cd yay
+#makepkg -si 
+#echo YAY installed
+#read a
+
+for PACKAGE in $(cat LAPTOP/LAPTOP_PACMAN_PACKAGES.lst)
+do
+	sudo pacman -S --noconfirm --needed $PACKAGE
+done
+
+echo pacman installed
+read a
+
+for PACKAGE in $(cat LAPTOP/LAPTOP_AUR_PACKAGES.lst)
+do
+	yay -S --noconfirm --needed $PACKAGE
+done
+
+echo yay installed
+read a
+
+cp -r /FILES/configs/filezilla /home/ray/.config/
 cp -r /FILES/configs/filezilla /home/ray/.config/
