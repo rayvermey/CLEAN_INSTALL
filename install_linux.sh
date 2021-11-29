244a2e6082d1ec9eff33cb2b13b747a3703a7825
Ray Vermey <rayvermey@gmail.com>
2021-11-29 19:08:55 +0100

    changed install

--- install_linux.sh
+++ install_linux.sh
@@ -71,8 +71,7 @@ mount /dev/${DISK}3 /mnt/home
 echo Copying files
 cp -ax / /mnt
 cp mkinitcpio.conf /mnt/etc
-cp -vaT /run/archiso/bootmnt/arch/boot/$(uname -m)/vmlinuz-linux /mnt/boot/vmlinuz-linux
-genfstab -U /mnt >> /mnt/etc/fstab
+cp -vaT /run/archiso/bootmnt/arch/boot/$(uname -m)/vmlinuz-linux /mnt/boot/vmlinuz-linux genfstab -U /mnt >> /mnt/etc/fstab
 
 cp b43-firmware-6.30.163.46-1-any.pkg.tar.zst /mnt/root 
 cp sudoers /mnt/etc
@@ -107,8 +106,7 @@ sudo pacman -Syu --noconfirm
 sleep 2
 
 echo Pacman Keys
-pacman-key --init
-pacman-key --populate archlinux
+pacman-key --init && pacman-key --populate archlinux
 sleep 3
 
 echo Installing yay
